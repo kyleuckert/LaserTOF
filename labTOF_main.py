109c66ebc9563036b9f893ef3470f7c742ab3d0e
Kyle Uckert <thekyleuckert@gmail.com>
2015-08-26 16:09:06 -0400

    commented L2MS_main.py
    added comments to main program

--- labTOF_main.py
+++ labTOF_main.py
@@ -48,27 +48,28 @@ class labTOF(Frame):
 		#delegate the creation of the user interface to the initUI() method
 		#self.parent.title("Lab TOF")
 		#self.pack(fill=BOTH, expand=1)
-		#self.data=[[1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5]]
 		self.fig = Figure(figsize=(4,4), dpi=100)
-		self.fig.subplots_adjust(bottom=0.15, left=0.15)
-		#fig.clf()
 		#fig = Figure(facecolor='white', edgecolor='white')
-		#self.ax = self.fig.add_subplot(111)
-		#plt.clf()
-		#self.ax.plot(self.data[0],self.data[1])
+		self.fig.subplots_adjust(bottom=0.15, left=0.15)
 
 		self.canvas = FigureCanvasTkAgg(self.fig, self)
 		self.toolbar = NavigationToolbar2TkAgg(self.canvas, self)
-		#canvas.show()
 		#canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True) 
+		
+		#list containing time domain values
 		self.time=[]
+		#list containing mass domain values
 		self.mass=[]
+		#flag to indicate whether time or mass is displayed
 		#time flag=0
 		#mass flag=1
 		self.time_mass_flag=0
+		#list containing intensity values
 		self.intensity=[]
+		#list containing calibration values. (0,0) is used as a default
 		self.cal_time=[0.0]
 		self.cal_mass=[0.0]
+		#calibration point id
 		self.cid=-99
 		self.initUI()
         
@@ -84,29 +85,30 @@ class labTOF(Frame):
 		
 		#the Frame widget (accessed via the delf attribute) in the root window
 		#it is expanded in both directions
+		#CHANGE all pack to grid layout
 		self.pack(fill=BOTH, expand=1)
 
 		menubar = Menu(self.parent)
 		self.parent.config(menu=menubar)
 		
+		#contains file menu items
 		fileMenu = Menu(menubar)
 		fileMenu.add_command(label="Open", command=self.onOpen)
 		fileMenu.add_command(label="Export File", command=self.onExport)
 		fileMenu.add_command(label="Quit", command=self.quit)
 		menubar.add_cascade(label="File", menu=fileMenu)
 
+		#contains calibration menu items
+		#"self" is needed to access items later (to change state)
 		self.calMenu=Menu(menubar)     
 		self.calMenu.add_command(label="Start New Calibration", command=self.onCalStart)
 		self.calMenu.add_command(label="Open Calibration File", command=self.onOpenCal)
+		#default state for save calibration is disabled
+		#state is enabled once a calibration is defined
 		self.calMenu.add_command(label="Save Calibration File", command=self.onSaveCal, state=DISABLED)
 		menubar.add_cascade(label="Calibration", menu=self.calMenu)        
 
-		
-		#self.columnconfigure(1, weight=1)
-		#self.columnconfigure(3, pad=7)
-		#self.rowconfigure(3, weight=1)
-		#self.rowconfigure(5, pad=7)
-
+		#generate figure (no data at first)
 		self.generate_figure([0,1], ' ')
 		
 		#create instance of the button widget
@@ -116,41 +118,45 @@ class labTOF(Frame):
 		quitButton.pack(side=RIGHT, padx=5, pady=5)
 		#quitButton.grid(row=5, column=5, padx=5)
 
+		#convert to time domain
 		timeButton = Button(self, text="Time Domain", command = self.time_domain)
-		#convertButton.grid(row=5, column=4, padx=5)
 		timeButton.pack(side=RIGHT, padx=5, pady=5)
 
+		#convert to mass domain
 		self.massButton = Button(self, text="Mass Domain", command = self.mass_domain, state=DISABLED)
 		self.massButton.pack(side=RIGHT, padx=5, pady=5)
 
+		#smooth spectrum
 		self.smoothButton = Button(self, text="Smooth", command = self.smooth_data, state=DISABLED)
 		self.smoothButton.pack(side=RIGHT, padx=5, pady=5)
 
+		#label peaks in figure
 		self.peakButton = Button(self, text="Label Peaks", command = self.label_peaks, state=DISABLED)
 		self.peakButton.pack(side=RIGHT, padx=5, pady=5)
 
+		#calibration
+		#add calibration point
 		self.calibrateButton = Button(self, text="Add Calibration Point", command = self.calibrate, state=DISABLED)
 		self.calibrateButton.pack(side=LEFT, padx=5, pady=5)
 
+		#finish calibration and convert to mass domain
 		self.finishcalButton = Button(self, text="Finish Calibration", command = self.finish_calibrate, state=DISABLED)
 		self.finishcalButton.pack(side=LEFT, padx=5, pady=5)
-		#self.entrytext=StringVar()
-		#self.xaxisText = Entry(textvariable=self.entrytext)
-		#self.xaxisText.pack(side=RIGHT, padx=5, pady=5)
-
-		#xaxisLabel = Label(self, text="x range")
-		#xaxisLabel.pack(side=RIGHT, padx=5, pady=5)
 		
 
+	#convert to mass domain
+	#disabled until calibration is defined
 	def mass_domain(self):
 		self.time_mass_flag=1
 		self.generate_figure([self.mass,self.intensity], 'mass (Da)')
-		
+	
+	#convert to time domain	
 	def time_domain(self):
 		self.time_mass_flag=0
+		#plot in micro seconds
 		self.generate_figure([[1E6*x for x in self.time],self.intensity], 'time ($\mu$s)')
-		#self.generate_figure([self.time,self.intensity], 'time ($\mu$s)')
 		
+	#smooth data
 	def smooth_data(self):
 		#smooth self.time/mass/int variables - permanant
 		#probably not a good idea
@@ -159,11 +165,7 @@ class labTOF(Frame):
 		#wait for dialog to close
 		self.top.wait_window(self.top)
 		smoothed_signal = scipy.signal.savgol_filter(self.intensity,self.window,self.poly)
-		#self.intensity=smoothed_signal.T
-		#self.intensity=self.intensity.tolist()
-		#self.mass=np.array(self.mass)
-		#self.time=np.array(self.time)
-		#print 'smoothed: ', len(smoothed_signal), type(smoothed_signal), type(smoothed_signal.tolist())
+		#convert from array to list
 		self.intensity=smoothed_signal.tolist()
 		#print 'intensity: ', len(self.intensity), type(self.intensity)
 		if self.time_mass_flag == 1:
@@ -173,28 +175,19 @@ class labTOF(Frame):
 			#print 'time: ', len(self.time), type(self.time)
 			self.time_domain()
 
-
+	#plot figure
 	def generate_figure(self, data, xaxis_title):
-		#if i!=0:
-			#fig.clf()
-			#canvas.get_tk_widget().delete("all")
-		#fig = Figure(figsize=(4,4), dpi=100)
-		#fig.clf()
-		#fig = Figure(facecolor='white', edgecolor='white')
+		#clear figure
 		plt.clf()
 		ax = self.fig.add_subplot(111)
+		#clear axis
 		ax.cla()
 		ax.plot(data[0],data[1])
 		ax.set_xlabel(xaxis_title)
 		ax.set_ylabel('intensity')
-		#self.canvas.get_tk_widget().delete("all")
-		#self.canvas = FigureCanvasTkAgg(self.fig, self)
-		#self.canvas._tkcanvas.delete()
 		self.canvas.show()
 		self.canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
-		#self.can=Canvas(self.root, width=500, height=250)
 		#self.can.grid(row=1, column=0, columnspan=6, rowspan=3, padx=5, sticky=E+W+S+N)
-		#self.can(f)
 
 		self.toolbar.update()
 		self.canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=True)
@@ -203,31 +196,32 @@ class labTOF(Frame):
 		#Canvas.delete("all")
 		#.destroy()
 
-
+	#open file
 	def onOpen(self):
-
+		#displays .txt files in browser window
+		#only reads time domain data files
 		ftypes = [('txt files', '*.txt')]
 		dlg = tkFileDialog.Open(self, filetypes = ftypes)
 		fl = dlg.show()
 
 		if fl != '':
 			data = self.readFile(fl)
-			#self.delete_figure()
-			#self.fig.clf()
 			self.time=data[0]
-			#self.mass=map(lambda x: 1.742E12*(x ** 2), self.time)
 			self.intensity=data[1]
+			#plots datat in time domain
 			self.time_domain()
 			#self.peakButton.config(state=NORMAL)
+			#allows for smoothing of data
 			self.smoothButton.config(state=NORMAL)
-			#self.generate_figure([self.time, self.intensity], 'time')
-			#self.txt.insert(END, data)
 
+	#export data to txt file
 	def onExport(self):
+		#ask for save file name
 		savefile = tkFileDialog.asksaveasfile(mode='wb', defaultextension=".txt")
 		# asksaveasfile return `None` if dialog closed with "cancel".
 		if savefile is None:
 			return
+		#export mass or time domain data depending on flag (what is plotted)
 		if self.time_mass_flag == 0:
 			x=self.time
 		else:
@@ -239,120 +233,147 @@ class labTOF(Frame):
 			savefile.write(' ')
 			savefile.write(str(y[i]))
 			savefile.write('\n')
-#		text2save = str(self.intensity)
-#		savefile.write(text2save)
 		savefile.close()
 
+	#save calibration file
 	def onSaveCal(self):
+		#ask for save file name
 		savefile = tkFileDialog.asksaveasfile(mode='wb', defaultextension=".txt")
 		if savefile is None:
 			return
-
+		
+		#write header information?
+		#date/time, file name
 		for i in range(len(self.cal_time)):
 			savefile.write(str(self.cal_time[i]))
 			savefile.write(' ')
 			savefile.write(str(self.cal_mass[i]))
 			savefile.write('\n')
-#		text2save = str(self.intensity)
-#		savefile.write(text2save)
 		savefile.close()
 		
+	#open previous calibration file
 	def onOpenCal(self):
+		#display .txt files in browser window
 		ftypes = [('txt files', '*.txt')]
 		dlg = tkFileDialog.Open(self, filetypes = ftypes)
 		filename = dlg.show()
 
 		if filename != '':
+			#list with time calibration value
 			calt_read=[]
+			#list with mass calibration value
 			calm_read=[]
 			file=open(filename,'r')
 			#header=file.readline()
 			for line in file:
 				#read each row - store data in columns
 				temp = line.split(' ')
-				#print temp
 				calt_read.append(float(temp[0]))
 				calm_read.append(float(temp[1].rstrip('/n')))
+			#store values in calibration lists
 			self.cal_time=calt_read
 			self.cal_mass=calm_read
+		#call finish_calibrate method to calibrate and plot data in mass domain
 		self.finish_calibrate()
 
+	#called when "Start New Calibration" is selected
 	def onCalStart(self):
+		#plot data in time domain (reset plot)
 		self.time_domain()
+		#set calibration button (add new, finish) to enabled state
 		self.calibrateButton.config(state=NORMAL)
 		self.finishcalButton.config(state=NORMAL)
 
+	#called when click is made in plotting environment during calibration
 	def on_click(self, event):	
 		temp_len=len(self.cal_time)
 		#print 'temp len: ', temp_len
 	
+		#when the click is made within the plotting window
 		if event.inaxes is not None:
 			#print 'clicked: ', event.xdata, event.ydata
-			#calibration=[event.xdata, event.ydata]
-			#return calibration
 			self.cal_time.append(event.xdata*1E-6)
+			#sets up dialog box for user to enter mass value
 			self.MassDialog(self.parent)
 
 		else:
 			print 'Clicked ouside axes bounds but inside plot window'		
+		#disconnect from calibration click event when a new calibration value is set
 		if len(self.cal_time) > temp_len:
 			self.canvas.mpl_disconnect(self.cid)
-		#print 'len cal: ', len(self.calx)
 
+	#called during calibration
+	#asks user for mass value associated with time value selected
 	def MassDialog(self, parent):
 		top = self.top = Toplevel(self.parent)
 		Label(top, text="Actual Mass Value:").pack()
 		
 		self.e=Entry(top)
 		self.e.pack(padx=5)
-		
+		#calls DialogOK to set mass value to input		
 		b=Button(top, text="OK", command=self.DialogOK)
 		b.pack(pady=5)
 	
+	#called when Smooth button is pressed
+	#asks user for smooth input data
 	def SmoothDialog(self, parent):
 		top = self.top = Toplevel(self.parent)
+		#window length for smoothing function
+		#must be greater than poly value, positive, odd (51)
 		Label(top, text="Window Length:").grid(row=0, column=0, sticky=W, padx=5, pady=5)
 		self.w=Entry(top)
 		self.w.grid(row=0, column=1, padx=5, pady=5)
 
+		#polynomial order
+		#must be less than window length (3)
 		Label(top, text="Polynomial Order:").grid(row=1, column=0, sticky=W, padx=5, pady=5)
 		self.p=Entry(top)
 		self.p.grid(row=1, column=1, padx=5, pady=5)
 		
+		#calls DialogSmoothOK to set these values
 		b=Button(top, text="OK", command=self.DialogSmoothOK)
 		b.grid(row=2, column=1, padx=5, pady=5)
-		
+	
+	#called from smooth dialog box within smooth routine
 	def DialogSmoothOK(self):
+		#sets user-defined window length
 		self.window=float(self.w.get())
+		#sets user-defined polynomail order
 		self.poly=float(self.p.get())
 		self.top.destroy()
 		
+	#called from mass dialog box within calibration routine
 	def DialogOK(self):
+		#sets user-defined mass calibration value to mass cal list
 		self.cal_mass.append(float(self.e.get()))
+		#closes dialog box
 		self.top.destroy()
-		
+	
+	#called from calibration routine
+	#stores user-selected point in cid	
 	def calibrate(self):
 		#user selects point
 		self.cid=self.canvas.mpl_connect('button_press_event', self.on_click)
 
-	
+	#called from calibration routine after "finish" button is pressed
 	def finish_calibrate(self):
+		#disables buttons until "start new calibration" is selected
 		self.calibrateButton.config(state=DISABLED)
 		self.finishcalButton.config(state=DISABLED)
+		#allows plotting in mass domain
 		self.massButton.config(state=NORMAL)
+		#allows user to save calibration file
 		self.calMenu.entryconfig("Save Calibration File", state=NORMAL)
 		
-		#fit line through cal points
+		#fit quadratic fuction through cal points
 		popt, pcov = curve_fit(func, self.cal_time, self.cal_mass)
 		#convert time to mass
-		#self.mass=map(lambda x: (popt[0]*(x ** 2))+popt[1], self.time)
 		self.mass[:] = [popt[0]*(x**2)*(1E10) + popt[1] for x in self.time]
+		#plots figure in mass domain
 		self.mass_domain()
 
+	#function for file input
 	def readFile(self, filename):
-
-		#f = open(filename, "r")
-		#text = f.read()
 		file=open(filename,'r')
 		#there is some extra formatting on the first line - delete this data
 		header=file.readline()
@@ -361,15 +382,14 @@ class labTOF(Frame):
 		header=file.readline()
 		header=file.readline()
 		#read each row in the file
+		#list for temporary time data
 		time=[]
+		#list for temporary intensity data
 		intensity=[]
 		for line in file:
 			#check file format:
 			if ',' in line:
 				#read each row - store data in columns
-				#columns=line.split()
-				#mass.append(float(columns[0]))
-				#intensity.append(float(columns[1]))
 				temp = line.split(',')
 				time.append(float(temp[0]))
 				intensity.append(float(temp[1].rstrip('/n')))
@@ -381,21 +401,21 @@ class labTOF(Frame):
 		data=[time, intensity]
 		return data
 
+	#future function to label peaks on figure in order to save figure as an image file with labels
 	def label_peaks(self):
 		#something goes here eventually
 		print 'this button does not work'
 
-
+#quadratic function for time to mass conversion
 def func(x, a, b):
 	return (1E10)*a*(x**2)+b
 
 
 
 def main():
-
 	#the root window is created
 	root = Tk()
-	
+
 	#The geometry() method sets a size for the window and positions it on the screen.
 	#The first two parameters are width and height of the window.
 	#The last two parameters are x and y screen coordinates. 
@@ -406,7 +426,6 @@ def main():
 	
 	#enter the main loop
 	root.mainloop()  
-
 
 if __name__ == '__main__':
 	main()  
